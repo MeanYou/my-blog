19c1097fec3af250ec8b14d8c3dce1851e7c5b77
MeanYou <464595889@qq.com>
2018-11-28 18:25:51 +0800

    新增

---
+++ src/store/common.js
@@ -0,0 +1,49 @@
+// common state
+const defaults = {
+  sidebar: {
+    visible: true
+  },
+  title: '',
+  layout: 'DefaultLayout',
+  error: {
+    code: null,
+    level: null,
+    message: ''
+  }
+}
+
+// app首次加载的全局模块
+export default {
+  namespaced: true,
+
+  state: Object.assign({}, defaults),
+
+  mutations: {
+    updateSidebar (state, options) {
+      state.sidebar = Object.assign({}, defaults.sidebar, options);
+    },
+
+    updateTitle (state, title) {
+      state.title = title
+    },
+
+    updateLayout (state, layout) {
+      state.layout = layout
+    },
+
+    error (state, options) {
+      state.error = Object.assign({}, defaults.error, options)
+    },
+
+    clear (state) {
+      state = Object.assign({}, defaults)
+    }
+  },
+
+  actions: {
+    clear ({ state, commit, rootState, dispatch }) {
+      commit('clear');
+      dispatch('auth/clear', {}, { root: true });
+    },
+  }
+}
